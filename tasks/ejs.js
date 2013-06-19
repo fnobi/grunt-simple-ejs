b8e19ca82584eab49df63cdf53d4da1e2324d20d
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-06-20 00:36:37 +0900

    最小構成で作成。

---
+++ tasks/ejs.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+    grunt.registerTask('ejs', 'process ejs template', function () {
+        var path = require('path'),
+            ejs = require('ejs'),
+
+            config = grunt.config('ejs'),
+            options = grunt.file.readYAML(config.options);
+
+        var files = [];
+
+        if (!config.template[0]) {
+            config.template = [config.template];
+        }
+
+        config.template.forEach(function (pattern) {
+            grunt.file.expandMapping(pattern, config.dest, {
+                rename: function (dest, matchedSrcPath, options) {
+                    return dest + path.basename(matchedSrcPath).replace(/\.ejs$/, '');
+                }
+            }).forEach(function (file) {
+                options.filename = file.src[0];
+
+                grunt.file.write(
+                    file.dest,
+                    ejs.render(grunt.file.read(file.src[0]), options),
+                    { encoding: 'utf8' }
+                );
+                console.log('[write] %s', file.dest);
+            });;
+        });
+    });
+};
